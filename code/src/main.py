efa4d124b200369dd303402ed7fd74bfc3155e00
kenta <khorie@sfu.ca>
2022-10-14 20:42:37 -0700

    finish 1.7

--- code/src/main.py
+++ code/src/main.py
@@ -5,10 +5,12 @@ cases_train = pd.read_csv('../data/cases_2021_train.csv')
 cases_test = pd.read_csv('../data/cases_2021_test.csv')
 location = pd.read_csv('../data/location_2021.csv')
 
+# 1.1
+
 if 'outcome' in cases_train:
     cases_train.groupby('outcome').size()
 
-    """Creating outcome_group values"""
+    # Creating outcome_group values
     cases_train.loc[cases_train['outcome'].isin(['Recovered', 'recovered']), 'outcome_group'] = 'recovered'
 
     cases_train.loc[cases_train['outcome'].isin(
@@ -25,7 +27,7 @@ if 'outcome' in cases_train:
 
     cases_train.groupby('outcome_group').size()
 
-    """Removing outcome column"""
+    # Removing outcome column
     cases_train = cases_train.drop(['outcome'], axis=1)
 
     cases_train.to_csv('../data/cases_2021_train.csv')
@@ -34,15 +36,15 @@ if 'outcome' in cases_train:
 else:
     print("'outcome' column already removed")
 
-#1.4
-#cleaning train data set
-#drop rows with missing age values
-cases_train.dropna(subset = ['age'], inplace = True)
-cases_test.dropna(subset = ['age'], inplace = True)
+# 1.4
+# cleaning train data set
+# drop rows with missing age values
+cases_train.dropna(subset=['age'], inplace=True)
+cases_test.dropna(subset=['age'], inplace=True)
 
 ratio_count = 0
 for x in cases_train.index:
-    age = cases_train.loc[x,'age']
+    age = cases_train.loc[x, 'age']
     if age[1:2] == ".":
         age = int(age[0:1])
     elif age[2:3] == ".":
@@ -55,19 +57,19 @@ for x in cases_train.index:
         age = int(age[0:1])
     else:
         age = int(age)
-    cases_train.loc[x,'age'] = age
+    cases_train.loc[x, 'age'] = age
 
-    if cases_train.loc[x,'sex'] != 'male' and cases_train.loc[x,'sex'] != 'female':
-        if ((ratio_count % 3) == 0):
-            cases_train.loc[x,'sex'] = 'female'
+    if cases_train.loc[x, 'sex'] != 'male' and cases_train.loc[x, 'sex'] != 'female':
+        if (ratio_count % 3) == 0:
+            cases_train.loc[x, 'sex'] = 'female'
         else:
-            cases_train.loc[x,'sex'] = 'male'
+            cases_train.loc[x, 'sex'] = 'male'
         ratio_count += 1
 
-#cleaning test data set
+# cleaning test data set
 ratio_count = 0
 for x in cases_test.index:
-    age = cases_test.loc[x,'age']
+    age = cases_test.loc[x, 'age']
     if age[1:2] == ".":
         age = int(age[0:1])
     elif age[2:3] == ".":
@@ -80,23 +82,23 @@ for x in cases_test.index:
         age = int(age[0:1])
     else:
         age = int(age)
-    cases_test.loc[x,'age'] = age
+    cases_test.loc[x, 'age'] = age
 
-    if cases_test.loc[x,'sex'] != 'male' and cases_test.loc[x,'sex'] != 'female':
-        if ((ratio_count % 3) == 0):
-            cases_test.loc[x,'sex'] = 'female'
+    if cases_test.loc[x, 'sex'] != 'male' and cases_test.loc[x, 'sex'] != 'female':
+        if (ratio_count % 3) == 0:
+            cases_test.loc[x, 'sex'] = 'female'
         else:
-            cases_test.loc[x,'sex'] = 'male'
+            cases_test.loc[x, 'sex'] = 'male'
         ratio_count += 1
 
-#1.5
-#get rid of outliers values for fatality rate > 100%
+# 1.5
+# get rid of outliers values for fatality rate > 100%
 for x in location.index:
     if location.loc[x, 'Case_Fatality_Ratio'] > 100:
         location.drop(x, inplace=True)
 
-#1.6
-#adding attributes to train data later for combinging with location data
+# 1.6
+# adding attributes to train data later for combining with location data
 cases_train["Confirmed"] = ''
 cases_train["Deaths"] = ''
 cases_train["Recovered"] = ''
@@ -104,7 +106,7 @@ cases_train["Active"] = ''
 cases_train["Incident_Rate"] = ''
 cases_train["Case_Fatality_Ratio"] = ''
 
-#adding attributes to test data later for combinging with location data
+# adding attributes to test data later for combining with location data
 cases_test["Confirmed"] = ''
 cases_test["Deaths"] = ''
 cases_test["Recovered"] = ''
@@ -112,45 +114,90 @@ cases_test["Active"] = ''
 cases_test["Incident_Rate"] = ''
 cases_test["Case_Fatality_Ratio"] = ''
 
-#clean up country name in location data file
+# clean up country name in location data file
 for y in location.index:
-    if location.loc[y,'Country_Region'] == 'Korea, South':
-        location.loc[y,'Country_Region'] = 'South Korea'
-    elif location.loc[y,'Country_Region'] == 'US':
-        location.loc[y,'Country_Region'] = 'United States'
+    if location.loc[y, 'Country_Region'] == 'Korea, South':
+        location.loc[y, 'Country_Region'] = 'South Korea'
+    elif location.loc[y, 'Country_Region'] == 'US':
+        location.loc[y, 'Country_Region'] = 'United States'
 
-#get rid of duplicate data for joining data later
+# imputing missing country values with province if it applies
+
+cases_train['country'] = cases_train['country'].fillna(cases_train['province'] + "*")
+cases_test['country'] = cases_test['country'].fillna(cases_test['province'] + "*")
+
+# print(location.info())
+
+# get rid of duplicate data for joining data later
 country = "place holder 1"
 for x in location.index:
-    if location.loc[x,'Country_Region'] == country:
-        location.drop(x, inplace = True)
+    if location.loc[x, 'Country_Region'] == country:
+        location.drop(x, inplace=True)
     else:
-        country = location.loc[x,'Country_Region']
+        country = location.loc[x, 'Country_Region']
 
+# print(location.info())
+
+
+count = 0
+print("Joining cases_train with Location...")
 for y in cases_train.index:
     for x in location.index:
         if location.loc[x, 'Country_Region'] == cases_train.loc[y, 'country']:
-            print(count)
+            # print(count)
             cases_train.loc[y, 'Confirmed'] = location.loc[x, 'Confirmed']
             cases_train.loc[y, 'Deaths'] = location.loc[x, 'Deaths']
             cases_train.loc[y, 'Recovered'] = location.loc[x, 'Recovered']
             cases_train.loc[y, 'Active'] = location.loc[x, 'Active']
             cases_train.loc[y, 'Incident_Rate'] = location.loc[x, 'Incident_Rate']
             cases_train.loc[y, 'Case_Fatality_Ratio'] = location.loc[x, 'Case_Fatality_Ratio']
+            # to check it's actually running
+            count += 1
             break
+print("Done")
 
+count = 0
+print("Joining cases_test with Location...")
 for y in cases_test.index:
     for x in location.index:
         if location.loc[x, 'Country_Region'] == cases_test.loc[y, 'country']:
-            print(count)
+            # print(count)
             cases_test.loc[y, 'Confirmed'] = location.loc[x, 'Confirmed']
             cases_test.loc[y, 'Deaths'] = location.loc[x, 'Deaths']
             cases_test.loc[y, 'Recovered'] = location.loc[x, 'Recovered']
             cases_test.loc[y, 'Active'] = location.loc[x, 'Active']
             cases_test.loc[y, 'Incident_Rate'] = location.loc[x, 'Incident_Rate']
             cases_test.loc[y, 'Case_Fatality_Ratio'] = location.loc[x, 'Case_Fatality_Ratio']
+            # to check it's actually running
+            count += 1
             break
+print("Done")
 
-cases_train.to_csv('cases_2021_test_processed.csv', encoding='utf-8', index=False)
-cases_test.to_csv('cases_2021_train_processed.csv', encoding='utf-8', index=False)
-location.to_csv('location_2021_processed.csv', encoding='utf-8', index=False)
+cases_train.to_csv('../results/cases_2021_test_processed.csv', encoding='utf-8', index=False)
+cases_test.to_csv('../results/cases_2021_train_processed.csv', encoding='utf-8', index=False)
+location.to_csv('../results/location_2021_processed.csv', encoding='utf-8', index=False)
+
+# remove above post 1.7
+
+# 1.7
+
+# getting files again
+cases_train = pd.read_csv('../results/cases_2021_train_processed.csv')
+cases_test = pd.read_csv('../results/cases_2021_test_processed.csv')
+
+# removing unnamed rows
+cases_train = cases_train.loc[:, ~cases_train.columns.str.contains('^Unnamed')]
+cases_test = cases_test.loc[:, ~cases_test.columns.str.contains('^Unnamed')]
+
+pd.set_option('display.max_columns', None)
+
+# removing column that are not considered relevant features for classification
+cases_train = cases_train.drop(['source', 'latitude', 'longitude', 'Deaths'], axis=1)
+cases_test = cases_test.drop(['source', 'latitude', 'longitude', 'Deaths'], axis=1)
+
+# create files for feature csv
+cases_train.to_csv('../results/cases_2021_test_processed_features.csv', encoding='utf-8', index=False)
+cases_test.to_csv('../results/cases_2021_train_processed_features.csv', encoding='utf-8', index=False)
+
+# print(cases_train.isna().sum())
+# print(cases_test.isna().sum())
